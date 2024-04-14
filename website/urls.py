bb26dbefdacc5f88f3e976c93c8efc16162987be
farhadvarnus <farhad.rouhollahi@gmail.com>
2024-04-14 22:32:12 +0330

    some bug fix and add extends
    bug fix
    add extends to index.html

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path("", index_view)
+    path("", index_view),
+    path("contact", contact_view),
+    path("about", about_view),
 ]
--- website/views.py
+++ website/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, "website/index.html")
+
+
+def about_view(request):
+    return render(request, "website/about.html")
+
+
+def contact_view(request):
+    return render(request, "website/contact.html")
