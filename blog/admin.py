44a1c2f71f455d594919b3aed86f4f1eb235af77
farhadvarnus <farhad.rouhollahi@gmail.com>
2024-04-18 13:18:22 +0330

    add post and contact to admin page
    add post and contact to admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    list_display = ("title", "status", "counted_view",
+                    "created_date", "updated_date")
+    list_filter = ("status",)
+    search_fields = ["title", "content"]
+
+
+admin.site.register(Post, PostAdmin)
